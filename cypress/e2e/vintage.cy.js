766c1b5a6bf9c1737f8ac7b8348f293bd127f2a7
Sabeer Sulaiman <sabeer@browserstack.com>
2023-08-11 09:52:47 +0530

    test: add percy for visual tests

--- cypress/e2e/vintage.cy.js
+++ cypress/e2e/vintage.cy.js
@@ -3,6 +3,7 @@ describe('template spec', () => {
     cy.visit('http://localhost:3000/index.html')
 
     cy.get('.logo-link').should('be.visible')
+    cy.percySnapshot('Home Page', {widths: [800, 1080]})
   })
 
   it('should visit services page', () => {
@@ -10,6 +11,7 @@ describe('template spec', () => {
 
     cy.get('#service-link').click()
     cy.get('.services .service').should('have.length', 5)
+    cy.percySnapshot('Services Page', {widths: [800, 1080]})
   })
 
   it('should visit contacts page', () => {
@@ -20,5 +22,6 @@ describe('template spec', () => {
     cy.get('#message').type('Message from User....{enter}new line')
 
     cy.get('.form .btn.blue').should('be.visible')
+    cy.percySnapshot('Contacts Page', {widths: [800, 1080]})
   })
 })
