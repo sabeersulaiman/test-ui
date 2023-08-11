ca67e122ba19df84130414d1a1e2dd0e74c31c8f
Sabeer Sulaiman <sabeer@browserstack.com>
2023-08-11 08:28:06 +0530

    test: add applitools

---
+++ applitools.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testConcurrency: 5,
+    batchName: 'Vitage Visual Tests',
+    browser: [
+        {width: 800, height: 600, name: 'chrome'},
+        {width: 1080, height: 900, name: 'chrome'}
+    ]
+}
--- cypress/e2e/vintage.cy.js
+++ cypress/e2e/vintage.cy.js
@@ -1,8 +1,25 @@
 describe('template spec', () => {
+  beforeEach(() => {
+    cy.eyesOpen({
+      appName: 'CNB Vintage Web',
+      testName: Cypress.currentTest.title
+    })
+  })
+
+  afterEach(() => {      
+    cy.eyesClose()
+  })
+
   it('should load home page logo', () => {
     cy.visit('http://localhost:3000/index.html')
 
     cy.get('.logo-link').should('be.visible')
+
+    cy.eyesCheckWindow({
+      tag: "Home page",
+      target: 'window',
+      fully: true
+    })
   })
 
   it('should visit services page', () => {
@@ -10,6 +27,13 @@ describe('template spec', () => {
 
     cy.get('#service-link').click()
     cy.get('.services .service').should('have.length', 5)
+
+
+    cy.eyesCheckWindow({
+      tag: "Services page",
+      target: 'window',
+      fully: true
+    })
   })
 
   it('should visit contacts page', () => {
@@ -20,5 +44,11 @@ describe('template spec', () => {
     cy.get('#message').type('Message from User....{enter}new line')
 
     cy.get('.form .btn.blue').should('be.visible')
+
+    cy.eyesCheckWindow({
+      tag: "Contact page",
+      target: 'window',
+      fully: true
+    })
   })
 })
